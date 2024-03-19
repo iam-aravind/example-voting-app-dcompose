fd186786d67d5f1bfc5dd876284193340ae51283
iam-aravind <94608208+iam-aravind@users.noreply.github.com>
2024-03-19 17:46:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,9 @@ provider "aws" {
   region = "us-east-1" # Specify your desired AWS region
 }
 
-resource "null_resource" "import_key_pair" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      aws ec2 import-key-pair \
-        --key-name AwsKeyName \
-        --public-key-material var.publickey
-    EOT
-  }
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-key-pair"  # Specify the name for your key pair
+  public_key = file(var.publickey)  # Access the public key path from a variable
 }
 
 variable "publickey" {
